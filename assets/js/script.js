fb7b5809d2916c6baf6ba327e6129d230eb4a9f5
Pete Machin <designer@deadpixelsdesign.com>
2021-11-22 22:09:39 +0000

    Add array for first image slide

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    const cardArray = [
+        {
+            name: '456',
+            img: 'assets/images/456.png'
+        },
+    ]
 })
