35d99c9a00473269f528131e0617e342a434cbdf
Pete Machin <designer@deadpixelsdesign.com>
2021-11-22 23:15:08 +0000

    Add game board the white slide to each tile in the grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+//create an array for a pair of all 10 image slides
+
     const cardArray = [
         {
             name: '456',
-            img: 'assets/images/player-456-100px.png'
+            img: 'images/player-456-100px.png'
         },
         {
             name: '456',
-            img: 'assets/images/player-456-100px.png'
+            img: 'images/player-456-100px.png'
         },
         {
             name: 'front man',
-            img: 'assets/images/front-man-100px.png'
+            img: 'images/front-man-100px.png'
         },
         {
             name: 'front man',
-            img: 'assets/images/front-man-100px.png'
+            img: 'images/front-man-100px.png'
         },
         {
             name: 'square bad guy',
-            img: 'assets/images/square-bad-guy-100px.png'
+            img: 'images/square-bad-guy-100px.png'
         },
         {
             name: 'square bad guy',
-            img: 'assets/images/square-bad-guy-100px.png'
+            img: 'images/square-bad-guy-100px.png'
         },
         {
             name: 'player 001',
-            img: 'assets/images/player-001-100px.png'
+            img: 'images/player-001-100px.png'
         },
         {
             name: 'player 001',
-            img: 'assets/images/player-001-100px.png'
+            img: 'images/player-001-100px.png'
         },
         {
             name: 'circle bad guy',
@@ -39,47 +41,65 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         {
             name: 'circle bad guy',
-            img: 'assets/images/circle-bad-guy-100px.png'
+            img: 'images/circle-bad-guy-100px.png'
         },
         {
             name: 'player 067',
-            img: 'assets/images/player-067-100px.png'
+            img: 'images/player-067-100px.png'
         },
         {
             name: 'player 067',
-            img: 'assets/images/player-067-100px.png'
+            img: 'images/player-067-100px.png'
         },
         {
             name: 'triangle bad guy',
-            img: 'assets/images/triangle-bad-guy-100px.png'
+            img: 'images/triangle-bad-guy-100px.png'
         },
         {
             name: 'triangle bad guy',
-            img: 'assets/images/triangle-bad-guy-100px.png'
+            img: 'images/triangle-bad-guy-100px.png'
         },
         {
             name: 'circle',
-            img: 'assets/images/circle-100px.png'
+            img: 'images/circle-100px.png'
         },
         {
             name: 'circle',
-            img: 'assets/images/circle-100px.png'
+            img: 'images/circle-100px.png'
         },
         {
             name: 'triangle',
-            img: 'assets/images/triangle-100px.png'
+            img: 'images/triangle-100px.png'
         },
         {
             name: 'triangle',
-            img: 'assets/images/triangle-100px.png'
-        },
-        {
-            name: 'triangle',
-            img: 'assets/images/triangle-b100px.png'
+            img: 'images/triangle-100px.png'
         },
         {
             name: 'square',
-            img: 'assets/images/square-100px.png'
+            img: 'images/square-100px.png'
+        },
+        {
+            name: 'square',
+            img: 'images/square-100px.png'
         }
     ]
-})
+
+
+const grid = document.querySelector('.grid');
+
+//start creating the game board
+
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        var card = document.createElement('img');
+        card.setAttribute('src', 'assets/images/white-100px.png');
+        card.setAttribute('data-id', i);
+        // card.addEventListener('click' slide tuning over)
+        grid.appendChild(card);
+    }
+}
+
+createBoard();
+
+});
