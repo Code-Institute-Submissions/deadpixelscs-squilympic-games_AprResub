2056af6d713786c27e7c096d05a09a0c4de5e295
Pete Machin <designer@deadpixelsdesign.com>
2021-11-22 22:16:12 +0000

    Add second image slide which is identical to the first one of player 456

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const cardArray = [
         {
             name: '456',
-            img: 'assets/images/456.png'
+            img: 'assets/images/456-100px.png'
         },
+        {
+            name: '456',
+            img: 'assets/images/456-100px.png'
+        },
+        {
+            name: 'front man',
+            img: 'assets/images/front-man-100px.png'
+        },
+        {
+            name: 'front man',
+            img: 'assets/images/front-man-100px.png'
+        },
+        
     ]
 })
