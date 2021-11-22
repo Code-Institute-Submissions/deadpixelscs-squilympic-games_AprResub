2845e031e1777e31ec92d8dcdf834d26e1029a5a
Pete Machin <designer@deadpixelsdesign.com>
2021-11-22 22:23:40 +0000

    Add remaining pairs of tiles until all 10 are complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const cardArray = [
         {
             name: '456',
-            img: 'assets/images/456-100px.png'
+            img: 'assets/images/player-456-100px.png'
         },
         {
             name: '456',
-            img: 'assets/images/456-100px.png'
+            img: 'assets/images/player-456-100px.png'
         },
         {
             name: 'front man',
@@ -17,6 +17,69 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'front man',
             img: 'assets/images/front-man-100px.png'
         },
-        
+        {
+            name: 'square bad guy',
+            img: 'assets/images/square-bad-guy-100px.png'
+        },
+        {
+            name: 'square bad guy',
+            img: 'assets/images/square-bad-guy-100px.png'
+        },
+        {
+            name: 'player 001',
+            img: 'assets/images/player-001-100px.png'
+        },
+        {
+            name: 'player 001',
+            img: 'assets/images/player-001-100px.png'
+        },
+        {
+            name: 'circle bad guy',
+            img: 'assets/images/circle-bad-guy-100px.png'
+        },
+        {
+            name: 'circle bad guy',
+            img: 'assets/images/circle-bad-guy-100px.png'
+        },
+        {
+            name: 'player 067',
+            img: 'assets/images/player-067-100px.png'
+        },
+        {
+            name: 'player 067',
+            img: 'assets/images/player-067-100px.png'
+        },
+        {
+            name: 'triangle bad guy',
+            img: 'assets/images/triangle-bad-guy-100px.png'
+        },
+        {
+            name: 'triangle bad guy',
+            img: 'assets/images/triangle-bad-guy-100px.png'
+        },
+        {
+            name: 'circle',
+            img: 'assets/images/circle-100px.png'
+        },
+        {
+            name: 'circle',
+            img: 'assets/images/circle-100px.png'
+        },
+        {
+            name: 'triangle',
+            img: 'assets/images/triangle-100px.png'
+        },
+        {
+            name: 'triangle',
+            img: 'assets/images/triangle-100px.png'
+        },
+        {
+            name: 'triangle',
+            img: 'assets/images/triangle-b100px.png'
+        },
+        {
+            name: 'square',
+            img: 'assets/images/square-100px.png'
+        }
     ]
 })
